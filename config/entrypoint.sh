995a6cefc3c99c7e4230444902202f28355940fa
Johannes ter Haak <jojo@terhaak.de>
2019-05-10 19:50:30 +0200

    automatic database schema upgrade

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -3,6 +3,7 @@
 mkdir -p /data/feed-icons
 
 if [ "x$MODE" == "xupdater" ]; then
+    echo -e "yes\n" | php /var/www/html/update.php --update-schema
     exec php /var/www/html/update.php --daemon
 else
     exec apache2ctl -DFOREGROUND
